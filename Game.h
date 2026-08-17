825f602ca6fc08b630d1c458a9c1735dedd2fd97
yihong2007 <yhlee1632@gmail.com>
2026-08-17 17:20:48 +0800

    Add files via upload
    Starting point of project
    Displaying of:
    - Main Menu
    - Class Selection
    - Game Map

---
+++ Game.h
@@ -0,0 +1,15 @@
+#pragma once
+class Game
+{
+    bool gameRunning;
+    char mapGrid[12][12];
+
+public: 
+    Game();
+    ~Game();
+    void Start();
+    void MainMenu();
+    void ClassSelection();
+    void displayGame();
+};
+
