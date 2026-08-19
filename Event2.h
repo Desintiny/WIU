ffbdb27f49967acde9da957442c09f2a01b4f523
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-19 14:53:27 +0800

    Update Event2.h

--- Event2.h
+++ Event2.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <vector>
 
 class Event2
 {
+private:
+    std::vector<int> usedEvents;
+    bool IsUsed(int event);
+
 public:
     void PathChoice();
     void ForestEvent(int event);
