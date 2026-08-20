eb1b3f496a6e7c95bfdcd7db914e0270b262a70c
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-20 11:31:31 +0800

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Entity.h"
+#include "Equipment.h"
+
+class Player : public Entity
+{
+	int staminaPts, maxStaminaPts;
+
+	int minRange;
+	int maxRange;
+
+	Equipment AttackRing;
+	Equipment HpRing;
+	Equipment SharkToothCharm;
+	Equipment IdolTrinket;
+	Equipment SilverBracelet;
+	Equipment WoodCarvedNecklace;
+	Equipment GemCharm;
+	Equipment TreeEmblem;
+
+public:
+	Player(string n);
+	~Player();
+
+	void PlayerMovement(char sym, char input, char mapGrid[12][12]);
+	bool PlayerAtkDirection(char input, int& targetRow, int& targetCol);
+
+	virtual void PlayerAttack(Entity* enemy);
+	virtual void EquipWeapon();
+
+	void setStamina(int);
+	int getStamina(void);
+
+	void setMaxStamina(int);
+	int getMaxStamina(void);
+
+	void setMinRange(int);
+	int getMinRange(void);
+
+	void setMaxRange(int);
+	int getMaxRange(void);
+
+	int GetAccuracy();	//Equipment Stats	
+	int GetEquipmentHealth();
+	int GetEquipmentDamage();
+
+	void GetAttackRing();	//Indiv equipment
+	void GetHpRing();
+	void GetSharkToothCharm();
+	void GetIdolTrinket();
+	void GetSilverBracelet();
+	void GetWoodCarvedNecklace();
+	void GetGemCharm();
+	void GetTreeEmblem();
+};
+
