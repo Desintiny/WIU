93f284484cf2bcaed8e77b5aca0f47acf4bd6810
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-20 12:04:28 +0800

    Refactor RNG class to include ability hit logic
    Added AbilityHitOrMiss method for ability attacks.

--- RNG.cpp
+++ RNG.cpp
@@ -1,5 +1,6 @@
 #include "RNG.h"
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,23 +9,38 @@ RNG::RNG() {
     Accuracy = 0;
     DidHit = false;
 }
-void RNG::SetAccuracy (int accuracy) {
+
+void RNG::SetAccuracy(int accuracy) {
     Accuracy = accuracy;
 }
 
-void RNG::HitOrMiss()
-{
-    GotHit = rand() % 101 + Accuracy;
+void RNG::HitOrMiss() {
+    // Basic attack: equipment accuracy applies
+    GotHit = rand() % 100 + Accuracy;
+
     if (GotHit >= 100) {
-        cout << "You have Hit the enemy (100%)" << endl;
+        cout << "You have Hit the enemy (100%)\n\n";
         DidHit = true;
     }
     else if (GotHit >= 31) {
-        cout << "You have Hit the enemy (" << GotHit << "%)" << endl;
+        cout << "You have Hit the enemy (" << GotHit << "%)\n\n";
         DidHit = true;
     }
     else {
-        cout << "You have missed (" << GotHit << "%)" << endl;
+        cout << "You have missed (" << GotHit << "%)\n\n";
+        DidHit = false;
+    }
+}
+
+void RNG::AbilityHitOrMiss(int chance) {
+    // Ability: equipment accuracy does NOT apply
+    int roll = rand() % 100;
+    if (roll < chance) {
+        cout << "Your Abiltity Hits. (" << chance << "%)\n\n";
+        DidHit = true;
+    }
+    else {
+        cout << "Your Ability missed. (" << chance << "%)\n\n";
         DidHit = false;
     }
 }
