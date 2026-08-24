adfcab8d62bd5c5408dd7bbba6ee30150ecf4fbb
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-24 13:10:37 +0800

    Add dodge chance to Archer class constructor

--- cpp/Archer.cpp
+++ cpp/Archer.cpp
@@ -2,27 +2,28 @@
 
 Archer::Archer(string n) : Player(n)
 {
-	static const int health = 28;
-	static const int stamina = 10;
+    static const int health = 28;
+    static const int stamina = 10;
+    static const int dodge = 7;
 
-	setHealth(health);
-	setMaxHealth(health);
-	
-	setStamina(stamina);
-	setMaxStamina(stamina);
+    setHealth(health);
+    setMaxHealth(health);
 
-	setAttack(7);
+    setStamina(stamina);
+    setMaxStamina(stamina);
 
-	setMinRange(1);
-	setMaxRange(4);
-};
+    setAttack(7);
+
+    setMinRange(1);
+    setMaxRange(4);
+
+    setDodgeChance(dodge);
+}
 
 Archer::~Archer()
 {
-
 }
 
 void Archer::EquipWeapon()
 {
-
 }
