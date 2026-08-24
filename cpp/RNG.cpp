db97d126f8c1b7f215e823b0fe291fd360f378de
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-24 13:12:38 +0800

    Implement dodge feature in RNG class
    Added dodge functionality and updated RNG class methods.

--- cpp/RNG.cpp
+++ cpp/RNG.cpp
@@ -4,55 +4,91 @@
 
 using namespace std;
 
-RNG::RNG() {
+RNG::RNG()
+{
     GotHit = 0;
     Accuracy = 0;
     DidHit = false;
+    Dodged = false;
 }
 
-void RNG::SetAccuracy(int accuracy) {
+void RNG::SetAccuracy(int accuracy)
+{
     Accuracy = accuracy;
 }
 
-void RNG::HitOrMiss() {
+void RNG::HitOrMiss()
+{
     // Basic attack: equipment accuracy applies
     GotHit = rand() % 100 + Accuracy;
 
-    if (GotHit >= 100) {
+    if (GotHit >= 100)
+    {
         cout << "You have Hit the enemy (100%)\n\n";
         DidHit = true;
     }
-    else if (GotHit >= 31) {
+    else if (GotHit >= 31)
+    {
         cout << "You have Hit the enemy (" << GotHit << "%)\n\n";
         DidHit = true;
     }
-    else {
+    else
+    {
         cout << "You have missed (" << GotHit << "%)\n\n";
         DidHit = false;
     }
 }
 
-void RNG::AbilityHitOrMiss(int chance) {
+void RNG::AbilityHitOrMiss(int chance)
+{
     // Ability: equipment accuracy does NOT apply
     int roll = rand() % 100;
-    if (roll < chance) {
-        cout << "Your Abiltity Hits. (" << chance << "%)\n\n";
+
+    if (roll < chance)
+    {
+        cout << "Your Ability Hits. (" << chance << "%)\n\n";
         DidHit = true;
     }
-    else {
+    else
+    {
         cout << "Your Ability missed. (" << chance << "%)\n\n";
         DidHit = false;
     }
 }
 
-bool RNG::GetDidHit() {
+void RNG::Dodge(int chance)
+{
+    int roll = rand() % 100;
+
+    if (roll < chance)
+    {
+        Dodged = true;
+        cout << "You dodged the attack! (" << chance << "%)\n\n";
+    }
+    else
+    {
+        Dodged = false;
+    }
+}
+
+bool RNG::GetDodged()
+{
+    return Dodged;
+}
+
+bool RNG::GetDidHit()
+{
     return DidHit;
 }
 
-bool RNG::CriticalHit(int chance) {
+bool RNG::CriticalHit(int chance)
+{
     int roll = rand() % 100;
-    if (roll < chance) {
+
+    if (roll < chance)
+    {
         return true;
     }
+
     return false;
 }
