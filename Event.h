391f8fe6c872b4a1b37fe7e624b4951299a93e7f
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-24 15:44:27 +0800

    Create Event class with event handling methods
    Added Event class with methods for handling events and healing.

--- Event.h
+++ Event.h
@@ -1,1 +1,19 @@
-Use Events.h in main
+#pragma once
+#include <vector>
+#include "Player.h"
+
+class Event
+{
+private:
+    std::vector<int> usedEvents;
+
+    bool IsUsed(int event);
+
+public:
+    void PathChoice(Player* player);
+    void ForestEvent(int event, Player* player);
+
+    // Heal helpers
+    void HealToMax(Player* player);
+    void Heal20(Player* player);
+};
