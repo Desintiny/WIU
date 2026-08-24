0e629775c5ef72680b75de82d9f074150fab8abf
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-24 13:08:14 +0800

    Add dodge chance to Mage constructor

--- cpp/Mage.cpp
+++ cpp/Mage.cpp
@@ -2,20 +2,23 @@
 
 Mage::Mage(string n) : Player(n)
 {
-	static const int health = 24;
-	static const int stamina = 10;
+    static const int health = 24;
+    static const int stamina = 10;
+    static const int dodge = 3;
 
-	setHealth(health);
-	setMaxHealth(health);
+    setHealth(health);
+    setMaxHealth(health);
 
-	setStamina(stamina);
-	setMaxStamina(stamina);
+    setStamina(stamina);
+    setMaxStamina(stamina);
 
-	setAttack(10);
+    setAttack(10);
 
-	setMinRange(1);
-	setMaxRange(3);
-};
+    setMinRange(1);
+    setMaxRange(3);
+
+    setDodgeChance(dodge);
+}
 
 Mage::~Mage()
 {
