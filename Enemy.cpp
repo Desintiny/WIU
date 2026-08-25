61b2e3ce7f4af68a83dec143e3436ba7c240ba2d
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-25 11:03:47 +0800

    Enhance EnemyAttack with dodge mechanics

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,6 +1,9 @@
 #include "Enemy.h"
+#include "Player.h"
+#include "RNG.h"
 #include <cstdlib>
 #include <iostream>
+
 using namespace std;
 
 Enemy::Enemy(string n) : Entity(n)
@@ -15,19 +18,29 @@ Enemy::~Enemy()
 {
 
 }
-
-
-
 void Enemy::EnemyAttack(Entity* target)
 {
 	if (target != nullptr)
 	{
+		RNG rng;
+		Player* player = dynamic_cast<Player*>(target);
+
+		if (player != nullptr)
+		{
+			rng.Dodge(player->getDodgeChance());
+
+			if (rng.GetDodged())
+			{
+				return;
+			}
+		}
+
 		int dmg = getAttack();
 
 		target->TakeDamage(dmg);
 
 		cout << getName() << " attacks " << target->getName()
-			 << " for " << dmg << " damage!" << endl;
+			<< " for " << dmg << " damage!" << endl;
 
 		if (!target->IsAlive())
 		{
@@ -36,7 +49,7 @@ void Enemy::EnemyAttack(Entity* target)
 		else
 		{
 			cout << target->getName() << " has "
-				 << target->getHealth() << " HP left." << endl;
+				<< target->getHealth() << " HP left." << endl;
 		}
 	}
 }
