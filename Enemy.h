21930fb0f663b8b754660ff8adc36c42d13bd88f
Wayne-Leow <260408X@mymail.nyp.edu.sg>
2026-08-26 09:44:56 +0800

    Add disabledTurns getter and setter methods
    Added getter and setter for disabledTurns in Enemy class.

--- Enemy.h
+++ Enemy.h
@@ -7,12 +7,16 @@ using namespace std;
 
 class Enemy : public Entity
 {
+private:
+	int disabledTurns;
 
 public:
 	Enemy(string n);
 	~Enemy();
 
 	virtual void EnemyMovement(Entity* target, char mapGrid[12][12]) = 0;
-
 	virtual void EnemyAttack(Entity* target);
+
+	int GetDisabledTurns();
+	void SetDisabledTurns(int turns);
 };
